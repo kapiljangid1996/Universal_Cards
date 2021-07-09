8a8cc75da0fb7733de311e71f5b38a8252aa5cf8
kapiljangid1996 <kapiljangid199276@gmail.com>
2021-07-09 10:04:16 +0530

    New

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
-{
-    //
+{    
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
 }
