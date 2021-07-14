3f4c10bafb4206168dd958f409e287328f76b798
kapiljangid1996 <kapiljangid199276@gmail.com>
2021-07-14 10:19:42 +0530

    New

--- app/helpers.php
+++ app/helpers.php
@@ -15,6 +15,21 @@ function front_menu($MenuPagemodel, $menu_id, $classes = '') {
 	return  $html;
 }
 
+function front_mobile_menu($MenuPagemodel, $menu_id, $classes = '') {
+
+	$html = '';
+
+	$main_ul_class = isset($classes['main_ul_class']) ? $classes['main_ul_class'] : '';
+
+	$html .= '<div class="mobile-navigation"> <nav> <ul class="mobile-menu">';
+
+	$html .= getMobileMenusHtml($MenuPagemodel, $menu_id, 0, 0, '', $classes);
+
+	$html .= '</ul> </nav> </div>';
+
+	return  $html;
+}
+
 function getMenusHtml($MenuPagemodel, $menu_id, $parent_id = 0, $lavel, $html='', $classes = '') {
 
 	if(!empty($menu_id)) {
@@ -121,7 +136,143 @@ function getMenusHtml($MenuPagemodel, $menu_id, $parent_id = 0, $lavel, $html=''
 	}
 }
 
+function getMobileMenusHtml($MenuPagemodel, $menu_id, $parent_id = 0, $lavel, $html='', $classes = '') {
+
+	if(!empty($menu_id)) {
+
+		$records = $MenuPagemodel::where('menu_id', $menu_id)->where('parent_id', $parent_id)->OrderBy('sort_number', 'ASC')->get();
+
+		if(!empty($records)) {
+			$i = 1;
+			$lavel = $lavel + 1;
+
+			foreach($records as $record) {
+				if ($record['mega_menu'] == 0) {
+					$menusArr  = array();
+
+					$menusArr['children'] = getChildMobileMenusList($MenuPagemodel, $menu_id, $record['id']);
+
+					$main_li_class= isset($classes['']) ? $classes[''] : '';
+
+					$target = ($record['new_tab'] == 1) ? '_blank' : '';
+
+					if(!empty($menusArr['children'])) {
+						$child_ul_class =  isset($classes['child_ul_class']) ? $classes['child_ul_class'] : '';
+						$main_li_class= isset($classes['main_li_class']) ? 'menu-item-has-children' : '';
+						$html .= '<li class="'.$main_li_class.'">';
+					}
+					else {
+						$html .= '<li>';	
+					}
+
+					$slug = $record['slug'];
+
+					$html .= '<a href="'.$slug.'" target="'.$target.'">';
+
+					$html .= $record['title'];
+
+					$html .= '</a>';
+
+					if(!empty($menusArr['children'])) { 
+						$sub_child_ul_class = ($lavel > 1) ? 'dropdown' : '';
+
+						$html .= '<ul class="'.$child_ul_class.$sub_child_ul_class.'">';
+
+						$html .= getMobileMenusHtml($MenuPagemodel, $menu_id, $record['id'], $lavel);
+
+						$html .= '</ul>';
+					}
+					else {
+						unset($menusArr['children']);
+					}
+
+					$html .= '</li>';
+
+					$i++;
+				}
+				else {
+					$menusArr  = array();
+
+					$menusArr['children'] = getChildMobileMenusList($MenuPagemodel, $menu_id, $record['id']);
+
+					$main_li_mega_menu_class = isset($classes['main_li_mega_menu_class']) ? $classes['main_li_mega_menu_class'] : '';
+
+					$target = ($record['new_tab'] == 1) ? '_blank' : '';
+
+					if(!empty($menusArr['children'])) {
+						$child_ul_mega_menu_class =  isset($classes['child_ul_mega_menu_class']) ? $classes['child_ul_mega_menu_class'] : '';
+						$main_li_class = isset($classes['main_li_class']) ? 'menu-item-has-children' : '';
+
+						$html .= '<li class="'.$main_li_class.'">';
+					}
+					else{
+						$html .= '<li class="'.$main_li_mega_menu_class.'">';
+					}					
+
+					$slug = $record['slug'];
+
+					$html .= '<a href="'.$slug.'" target="'.$target.'">';
+
+					$html .= $record['title'];
+
+					$html .= '</a>';
+
+					if(!empty($menusArr['children'])) { 
+						$sub_child_ul_mega_menu_class = ($lavel > 1) ? '' : '';
+
+						$html .= '<ul class="'.$child_ul_mega_menu_class.$sub_child_ul_mega_menu_class.'">';
+
+						$html .= getMobileMenusHtml($MenuPagemodel, $menu_id, $record['id'], $lavel);
+
+						$html .= '</ul>';
+					}
+					else {
+						unset($menusArr['children']);
+					}
+
+					$html .= '</li>';
+
+					$i++;
+				}
+			}
+		}
+
+		return $html;
+	}
+}
+
 function getChildMenusList($MenuPagemodel, $menu_id, $parent_id = 0) {
+
+	$menus = array();
+
+	if(!empty($menu_id)) {
+
+		$records = $MenuPagemodel::where('menu_id', $menu_id)->where('parent_id', $parent_id)->OrderBy('sort_number', 'ASC')->get();
+
+		if(!empty($records)) {
+			$i = 1;
+
+			foreach($records as $record) {
+				$menusArr  = array();				
+				$menusArr['id'] = $record['id'];
+				$menusArr['text'] = $record['title'];
+				$menusArr['href'] = $record['slug'];
+				$menusArr['page_type'] = $record['page_type'];
+				$menusArr['parent_id'] = $record['parent_id'];
+				$menusArr['sort_number'] = $record['sort_number'];
+				$menus[$record['id']] = $menusArr;
+				$i++;
+			}
+		}
+
+		$menus = !empty($menus) ? array_values($menus) : '';
+
+		return $menus;
+
+	}
+}
+
+function getChildMobileMenusList($MenuPagemodel, $menu_id, $parent_id = 0) {
 
 	$menus = array();
 
