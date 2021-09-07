b72ad189a035d78760f764053d8f8baa4a6f1bbf
deepak <deepak.tanwar@icon-solutions.us>
2021-09-07 18:13:10 +0530

    new

--- database/migrations/2021_09_07_051805_create_sites_table.php
+++ database/migrations/2021_09_07_051805_create_sites_table.php
@@ -15,6 +15,21 @@ class CreateSitesTable extends Migration
     {
         Schema::create('sites', function (Blueprint $table) {
             $table->id();
+            $table->string('logo')->nullable();
+            $table->string('slogan')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('email_address')->nullable();
+            $table->longtext('address')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('youtube')->nullable();
+            $table->longtext('footer_about')->nullable();
+            $table->string('footer_text')->nullable();
+            $table->string('meta_name')->nullable();
+            $table->longtext('meta_keyword')->nullable();
+            $table->longtext('meta_description')->nullable();
+            $table->longtext('google_analyst')->nullable();
             $table->timestamps();
         });
     }
